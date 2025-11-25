cffe495df53913a572543f3f7259de436d9bef9c
Meibee1987 <azka.mbipb@gmail.com>
2025-11-25 22:45:23 +0700

    update fix login page

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://ohbbyuupetxypdrhghyk.supabase.co";
-export const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9oYmJ5dXVwZXR4eXBkcmhnaHlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc1ODcxNjksImV4cCI6MjA3MzE2MzE2OX0.tZAHvuDyLy4t_nsTx9mc-idMHXu4rpiUAVnlfRA67Ew";
+export const supabaseUrl =import.meta.env.VITE_SUPABASE_URL;
+export const supabaseKey =import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
